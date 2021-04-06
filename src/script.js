0f08061d99205d8e2002d5852eec07b8a501ad15
Marvyn <keusmar@sjb-liege.org>
2021-04-06 17:18:06 +0200

    base commit

--- src/script.js
+++ src/script.js
@@ -3,34 +3,91 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
 
+// Loading
+const textureLoader = new THREE.TextureLoader();
+
+const normalTexture = textureLoader.load('/textures/height.png');
+
 // Debug
-const gui = new dat.GUI()
+const gui = new dat.GUI();
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl');
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Objects
-const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const sphereGeometry = new THREE.SphereBufferGeometry(.5, 64, 64);
 
 // Materials
 
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+const material = new THREE.MeshStandardMaterial();
+material.metalness = 0.7;
+material.roughness = 0.2;
+material.normalMap = normalTexture;
+
+material.color = new THREE.Color(0x292929);
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+const sphere = new THREE.Mesh(sphereGeometry, material);
+scene.add(sphere);
 
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
+const pointLight = new THREE.PointLight(0xffffff, 0.1);
+pointLight.position.x = 2;
+pointLight.position.y = 3;
+pointLight.position.z = 4;
+scene.add(pointLight);
+
+const pointLight2 = new THREE.PointLight(0xff0000, 0.2);
+pointLight2.position.set(-1.86, 1, -1.65);
+pointLight2.intensity = 10;
+scene.add(pointLight2);
+
+const light1 = gui.addFolder('Light 1');
+
+light1.add(pointLight2.position, 'x').min(-3).max(3).step(0.01);
+light1.add(pointLight2.position, 'y').min(-6).max(6).step(0.01);
+light1.add(pointLight2.position, 'z').min(-3).max(3).step(0.01);
+light1.add(pointLight2, 'intensity').min(0).max(10).step(0.01);
+
+let light1Color = {
+    color: 0xff0000,
+};
+
+light1.addColor(light1Color, 'color')
+    .onChange(() => {
+        pointLight2.color.set(light1Color.color);
+    });
+
+// const pointLightHelper = new THREE.PointLightHelper(pointLight2, 1);
+// scene.add(pointLightHelper);
+
+const pointLight3 = new THREE.PointLight(0xe1ff, 0.2);
+pointLight3.position.set(2.13, -3, -1.98);
+pointLight3.intensity = 6.8;
+scene.add(pointLight3);
+
+const light2 = gui.addFolder('Light 2');
+
+light2.add(pointLight3.position, 'x').min(-3).max(3).step(0.01);
+light2.add(pointLight3.position, 'y').min(-6).max(6).step(0.01);
+light2.add(pointLight3.position, 'z').min(-3).max(3).step(0.01);
+light2.add(pointLight3, 'intensity').min(0).max(10).step(0.01);
+
+const light2Color = {
+    color: 0xff0000,
+};
+
+light2.addColor(light2Color, 'color')
+    .onChange(() => {
+        pointLight3.color.set(light2Color.color);
+    });
+
+// const pointLightHelper2 = new THREE.PointLightHelper(pointLight3, 1);
+// scene.add(pointLightHelper2);
 
 /**
  * Sizes
@@ -40,30 +97,29 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
 
     // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
 
     // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 })
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
+scene.add(camera);
 
 // Controls
 // const controls = new OrbitControls(camera, canvas)
@@ -73,33 +129,60 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true,
 })
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
 
-const clock = new THREE.Clock()
+let mouseX = 0;
+let mouseY = 0;
 
-const tick = () =>
-{
+let targetX = 0;
+let targetY = 0;
 
-    const elapsedTime = clock.getElapsedTime()
+const windowHalfX = window.innerWidth / 2;
+const windowHalfY = window.innerHeight / 2;
+
+function onDocumentMouseMove(event) {
+    mouseX = (event.clientX - windowHalfX);
+    mouseY = (event.clientY - windowHalfY);
+}
+
+function updateSphere(event) {
+    sphere.position.y = window.scrollY * 0.001;
+}
+window.addEventListener('scroll', updateSphere);
+document.addEventListener('mousemove', onDocumentMouseMove);
+
+const clock = new THREE.Clock();
+
+const tick = () => {
+
+    targetX = mouseX * .001;
+    targetY = mouseY * .001;
+
+    const elapsedTime = clock.getElapsedTime();
 
     // Update objects
-    sphere.rotation.y = .5 * elapsedTime
+    sphere.rotation.y = .5 * elapsedTime;
+
+    sphere.rotation.x += .5 * (targetY - sphere.rotation.x);
+    sphere.rotation.y += .5 * (targetX - sphere.rotation.y);
+    sphere.position.z += .5 * (targetY - sphere.rotation.x);
 
     // Update Orbital Controls
     // controls.update()
 
     // Render
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 
     // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    window.requestAnimationFrame(tick);
 }
 
-tick()
+tick();
